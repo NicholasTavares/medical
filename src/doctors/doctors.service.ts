f14a7e92b47916d8e64c24c154e0cd1ca53c7d1e
NicholasTavares <nicholasbalby@hotmail.com>
2022-01-12 16:40:36 -0300

    feat: findAll service

--- src/doctors/doctors.service.ts
+++ src/doctors/doctors.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Doctor } from './entities/doctor.entity';
 
 @Injectable()
-export class DoctorsService {}
+export class DoctorsService {
+  constructor(
+    @InjectRepository(Doctor)
+    private readonly doctorRepository: Repository<Doctor>,
+  ) {}
+
+  async findAll() {
+    return this.doctorRepository.find();
+  }
+}

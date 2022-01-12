0e51b0da92bf5559d072942bbcc1a45cd3cae22d
NicholasTavares <nicholasbalby@hotmail.com>
2022-01-12 16:49:30 -0300

    feat: findOne and Create doctor service

--- src/doctors/doctors.service.ts
+++ src/doctors/doctors.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateDoctorDto } from './dto/createDoctor.dto';
 import { Doctor } from './entities/doctor.entity';
 
 @Injectable()
@@ -12,5 +13,21 @@ export class DoctorsService {
 
   async findAll() {
     return this.doctorRepository.find();
+  }
+
+  async findOne(id: string) {
+    const doctor = await this.doctorRepository.findOne(id);
+
+    if (!doctor) {
+      throw new NotFoundException(`Doctor ID of ${id} not found`);
+    }
+
+    return doctor;
+  }
+
+  create(createDoctorDto: CreateDoctorDto) {
+    const doctor = this.doctorRepository.create(createDoctorDto);
+
+    return this.doctorRepository.save(doctor);
   }
 }

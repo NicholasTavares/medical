1f49f280430a6e6b468a9ab553205dbabfa9909b
NicholasTavares <nicholasbalby@hotmail.com>
2022-01-13 12:47:51 -0300

    refactor: add dated to entity doctor

--- src/doctors/entities/doctor.entity.ts
+++ src/doctors/entities/doctor.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('doctors')
 export class Doctor {
@@ -22,4 +29,13 @@ export class Doctor {
 
   @Column()
   specialty: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
 }
